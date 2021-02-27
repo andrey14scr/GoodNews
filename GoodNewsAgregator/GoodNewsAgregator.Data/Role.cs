8c6322e2fec87a31f0dbec5f39ab7debdcf617ed
andrey14scr <70211433+andrey14scr@users.noreply.github.com>
2021-02-27 20:38:06 +0300

    added additional entity classes

---
+++ GoodNewsAgregator/GoodNewsAgregator.Data/Role.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoodNewsAgregator.Data
+{
+    class Role
+    {
+        public byte Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ GoodNewsAgregator/GoodNewsAgregator.Data/Source.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoodNewsAgregator.Data
+{
+    class Source
+    {
+        public short Id { get; set; }
+        public string Url { get; set; }
+    }
+}
