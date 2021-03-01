8efecb0ab106384448be0a37a7dd99545a658c85
andrey14scr <70211433+andrey14scr@users.noreply.github.com>
2021-03-01 18:42:09 +0300

    very simple main page with content created

--- GoodNewsAgregator/GoodNewsAgregator/Controllers/NavigationController.cs
+++ GoodNewsAgregator/GoodNewsAgregator/Controllers/NavigationController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GoodNewsAgregator.Data;
+
+using Microsoft.AspNetCore.Mvc;
 
 using System;
 using System.Collections.Generic;
@@ -11,7 +13,11 @@ namespace GoodNewsAgregator.Controllers
     {
         public IActionResult Main()
         {
-            return View();
+            List<Article> list = new List<Article>();
+            list.Add(new Article() { Id = Guid.NewGuid(), Content = "blabla1", Date = DateTime.Now, GoodFactor = 0.4f, SourceId = 1, Title = "Title1" });
+            list.Add(new Article() { Id = Guid.NewGuid(), Content = "blabla2", Date = DateTime.Now, GoodFactor = 0.6f, SourceId = 2, Title = "Title2" });
+            list.Add(new Article() { Id = Guid.NewGuid(), Content = "blabla3", Date = DateTime.Now, GoodFactor = 0.8f, SourceId = 3, Title = "Title3" });
+            return View(list);
         }
 
         public IActionResult Article()
---
+++ GoodNewsAgregator/GoodNewsAgregator/Models/NewsListModel.cs
@@ -0,0 +1,14 @@
+﻿using GoodNewsAgregator.Data;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoodNewsAgregator.Models
+{
+    public class NewsListModel
+    {
+        public IEnumerator<Article> list { get; set; }
+    }
+}
