7073d18e7f7968db66d706a379dd2a33fd108182
andrey14scr <70211433+andrey14scr@users.noreply.github.com>
2021-03-01 18:19:13 +0300

    create simple pages

---
+++ GoodNewsAgregator/GoodNewsAgregator/Controllers/NavigationController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoodNewsAgregator.Controllers
+{
+    public class NavigationController : Controller
+    {
+        public IActionResult Main()
+        {
+            return View();
+        }
+
+        public IActionResult Article()
+        {
+            return View();
+        }
+    }
+}
