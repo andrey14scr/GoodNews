a70e8672bcb4052b26d7ae5fe17946b4f70996c8
andrey14scr <70211433+andrey14scr@users.noreply.github.com>
2021-04-07 19:35:06 +0300

    logger added

--- GoodNewsAggregator/GoodNewsAggregator/Program.cs
+++ GoodNewsAggregator/GoodNewsAggregator/Program.cs
@@ -7,6 +7,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Serilog;
+using Serilog.Events;
+using System.Reflection;
+using System.IO;
 
 namespace GoodNewsAggregator
 {
@@ -14,11 +18,21 @@ namespace GoodNewsAggregator
     {
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .Enrich.FromLogContext()
+            .WriteTo.Console(LogEventLevel.Information)
+            .WriteTo.File(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\Logs\\log.log", LogEventLevel.Warning)
+            .CreateLogger();
+
+            Log.Information("Starting web host");
+
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+            .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
