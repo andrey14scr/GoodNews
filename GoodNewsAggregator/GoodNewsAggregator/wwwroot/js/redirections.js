14440883834725345f5058a1ec0611f3a0842f76
andrey14scr <70211433+andrey14scr@users.noreply.github.com>
2021-06-22 23:54:24 +0300

    ACCOUNT BTN IT IS WORKED

---
+++ GoodNewsAggregator/GoodNewsAggregator/wwwroot/js/redirections.js
@@ -0,0 +1,15 @@
+﻿function myAccount() {
+    window.location.href = "/Account/MyAccount";
+}
+
+function exit() {
+    document.getElementById("exitForm").submit();
+}
+
+function login() {
+    window.location.href = "/Account/Login";
+}
+
+function register() {
+    window.location.href = "/Account/Register";
+}
