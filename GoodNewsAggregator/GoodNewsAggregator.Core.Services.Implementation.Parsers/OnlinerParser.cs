35016c6e5e748509290c4a987bbbc540aa0a45e7
andrey14scr <70211433+andrey14scr@users.noreply.github.com>
2021-06-23 17:43:49 +0300

    adde slides for carousel

--- GoodNewsAggregator/GoodNewsAggregator.Core.Services.Implementation.Parsers/OnlinerParser.cs
+++ GoodNewsAggregator/GoodNewsAggregator.Core.Services.Implementation.Parsers/OnlinerParser.cs
@@ -27,8 +27,8 @@ namespace GoodNewsAggregator.Core.Services.Implementation.Parsers
                 if (node.Name == "p" && (node.InnerText.Contains("Читайте также:") || node.InnerText.Contains("Пишите нам:")))
                     break;
 
-                if (node.Name == "p" && node.Attributes.Count == 0 || 
-                    node.Name == "h2" && node.ChildNodes[0].Name != "a" || 
+                if (node.Name == "p" && node.Attributes.Count == 0 ||
+                    node.Name == "h2" && node.ChildNodes[0].Name != "a" && !node.InnerHtml.Contains("catalog.onliner.by") ||
                     node.Name == "h3" || node.Name == "h4" || node.Name == "h5" || node.Name == "h6" ||
                     node.Name == "ul")
                 {
