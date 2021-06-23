8b960316613d31288b5d0c1772b8d50f4a687eeb
andrey14scr <70211433+andrey14scr@users.noreply.github.com>
2021-06-23 13:41:21 +0300

    carousel block made

--- GoodNewsAggregator/GoodNewsAggregator/wwwroot/js/site.js
+++ GoodNewsAggregator/GoodNewsAggregator/wwwroot/js/site.js
@@ -1,4 +1,12 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function () {
 
-// Write your JavaScript code.
+
+    console.log($('#carouselwithIndicators').css('height')); // check the initial height of the carousel;
+
+    // now apply this height as a max-height on all the image items; css will handle the rest;
+    $('#carouselwithIndicators').find('.carousel-item img')
+        .css('max-height', $('#carouselwithIndicators').css('height'));
+
+    $('#carouselwithIndicators').find('.carousel-item img')
+        .css('min-height', $('#carouselwithIndicators').css('height'));
+});
